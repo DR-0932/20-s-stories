bf6839a5ed8b69b5dea044d8613f87db5d506e8c
DR-0932 <dhruvrajput757@example.com>
2025-07-10 22:55:41 +0530

    Switched to bcryptjs for Render deployment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const connectDB = require("./server/config/db");
 const { isActiveRoute } = require("./server/helpers/routeHelpers");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
